174937938586a74ec6a7920f08fe1231bba5528d
i.gura <gura.ilya2011@yandex.ru>
2022-02-08 22:54:09 +0300

    init

---
+++ TodoApp/Context/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Context
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<Todo>? Todos { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
