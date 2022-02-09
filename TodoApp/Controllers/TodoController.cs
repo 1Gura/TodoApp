d92cbf38997bd544ce0bf60d5b4a58d94601adf7
i.gura <gura.ilya2011@yandex.ru>
2022-02-09 20:41:12 +0300

    init

--- TodoApp/Controllers/TodoController.cs
+++ TodoApp/Controllers/TodoController.cs
@@ -18,8 +18,10 @@ namespace TodoApp.Controllers
         }
 
         [HttpGet]
-        public  async Task<ActionResult<Todo>> GetTodosAsync()
+        public async Task<ActionResult<Todo>> GetTodosAsync()
         {
+            _context.Todos.Add(new Todo() { Description = "awsdas", Title = "asdasd", Done = false });
+            await _context.SaveChangesAsync();
             return Ok(await _context.Todos.ToListAsync());
         }
     }
